d6493741040fc7c09d74c32df7ef234d56edcbf5
Shafira-van <shafiravandeyli10@gmail.com>
2022-10-17 14:55:53 +0700

    mengambil file json

--- home.js
+++ home.js
@@ -37,16 +37,84 @@ let getData = async () => {
 
 getData();
 
+let col1 = document.querySelector(".carousel-inner #row1");
+let col2 = document.querySelector(".carousel-inner #row2");
+let col3 = document.querySelector(".carousel-inner #row3");
 
 let getData2 = async () => {
   let response = await fetch(
-    `https://raw.githubusercontent.com/Shafira-van/MuseumAsia/dev/developer.json`
+    `https://634aa22e5df9528514155265.mockapi.io/webDev/fe28/carousel`
   );
   let movie2 = await response.json();
-
+  let print2 = "";
   console.log(movie2);
-  
+  movie2.splice(0,4).forEach((item) => {
+    console.log(item);
+    print2 += `<div class="col">
+    <div class="card" style="width: 14rem">
+      <img src="${item.img}" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">${item.object_name}</h5>
+        <p class="card-text">
+        ${item.description}
+        </p>
+        <p class="card-text">
+        ${item.provenience}
+        </p>
+        <a href="${item.url}" class="btn btn-primary">Click Here</a>
+      </div>
+    </div>
+  </div>`;
+    col1.innerHTML = print2;
+  });
 
+  let print3=""
+  console.log(movie2);
+  movie2.splice(0,4).forEach((item) => {
+    console.log(item);
+    print3 += `<div class="col">
+    <div class="card" style="width: 14rem">
+      <img src="${item.img}" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">${item.object_name}</h5>
+        <p class="card-text">
+        ${item.description}
+        </p>
+        <p class="card-text">
+        ${item.provenience}
+        </p>
+        <a href="${item.url}" class="btn btn-primary">Click Here</a>
+      </div>
+    </div>
+  </div>`;
+    col2.innerHTML = print3;
+  });
+
+  let print4=""
+  console.log(movie2);
+  movie2.splice(0,4).forEach((item) => {
+    console.log(item);
+    print4 += `<div class="col">
+    <div class="card" style="width: 14rem">
+      <img src="${item.img}" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">${item.object_name}</h5>
+        <p class="card-text">
+        ${item.description}
+        </p>
+        <p class="card-text">
+        ${item.provenience}
+        </p>
+        <a href="${item.url}" class="btn btn-primary">Click Here</a>
+      </div>
+    </div>
+  </div>`;
+    col3.innerHTML = print4;
+  });
+ 
+
+
+  
 };
 
 getData2();
